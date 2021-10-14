6d4f745fbc4f11b7d05e1cf5d541a04b67faef5e
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-14 11:39:53 +0530

    Update Model.java

--- CoffeeOrdering/app/src/main/java/com/example/coffeeordering/Model.java
+++ CoffeeOrdering/app/src/main/java/com/example/coffeeordering/Model.java
@@ -1,4 +1,26 @@
 package com.example.coffeeordering;
 
 public class Model {
+
+    String mDrinkName;
+    String mDrinkDetail;
+    int mDrinkPhoto;
+
+    public Model(String mDrinkName, String mDrinkDetail, int mDrinkPhoto) {
+        this.mDrinkName = mDrinkName;
+        this.mDrinkDetail = mDrinkDetail;
+        this.mDrinkPhoto = mDrinkPhoto;
+    }
+
+    public String getmDrinkName() {
+        return mDrinkName;
+    }
+
+    public String getmDrinkDetail() {
+        return mDrinkDetail;
+    }
+
+    public int getmDrinkPhoto() {
+        return mDrinkPhoto;
+    }
 }
