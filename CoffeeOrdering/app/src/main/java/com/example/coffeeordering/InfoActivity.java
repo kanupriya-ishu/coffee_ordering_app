cdb2f365eb784cdeb3e8b93116dce20744bc1326
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-15 12:28:20 +0530

    Update InfoActivity.java

--- CoffeeOrdering/app/src/main/java/com/example/coffeeordering/InfoActivity.java
+++ CoffeeOrdering/app/src/main/java/com/example/coffeeordering/InfoActivity.java
@@ -1,13 +1,70 @@
 package com.example.coffeeordering;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class InfoActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    ImageButton plusquantity, minusquantity;
+    TextView quantitynumber, drinnkName, coffeePrice;
+    CheckBox addToppings, addExtraCream;
+    Button addtoCart;
+    int quantity;
+    public Uri mCurrentCartUri;
+    boolean hasAllRequiredValues = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        imageView = findViewById(R.id.imageViewInfo);
+        plusquantity = findViewById(R.id.addquantity);
+        minusquantity  = findViewById(R.id.subquantity);
+        quantitynumber = findViewById(R.id.quantity);
+        drinnkName = findViewById(R.id.drinkNameinInfo);
+        coffeePrice = findViewById(R.id.coffeePrice);
+        addToppings = findViewById(R.id.addToppings);
+        addtoCart = findViewById(R.id.addtocart);
+        addExtraCream = findViewById(R.id.addCream);
+
+        // setting the name of drink
+        drinnkName.setText("Green Tea");
+
+        plusquantity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantity++;
+                displayQuantity();
+            }
+        });
+
+        minusquantity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int basePrice = 5;
+                // because we dont want the quantity go less than 0
+                if (quantity == 0) {
+                    Toast.makeText(InfoActivity.this, "Cant decrease quantity < 0", Toast.LENGTH_SHORT).show();
+                } else {
+                    quantity--;
+                    displayQuantity();
+                }
+            }
+        });
+    }
+
+    private void displayQuantity() {
+        quantitynumber.setText(String.valueOf(quantity));
     }
 }
