dc2fd30b85ce0b07199f0b1c44df1eb997e08149
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-15 17:36:47 +0530

    Updated onCreateLoader method

--- CoffeeOrdering/app/src/main/java/com/example/coffeeordering/InfoActivity.java
+++ CoffeeOrdering/app/src/main/java/com/example/coffeeordering/InfoActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -156,10 +157,21 @@ public class InfoActivity extends AppCompatActivity implements LoaderManager.Loa
         quantitynumber.setText(String.valueOf(quantity));
     }
 
-    @NonNull
     @Override
-    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
-        return null;
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {OrderContract.OrderEntry._ID,
+                OrderContract.OrderEntry.COLUMN_NAME,
+                OrderContract.OrderEntry.COLUMN_PRICE,
+                OrderContract.OrderEntry.COLUMN_QUANTITY,
+                OrderContract.OrderEntry.COLUMN_CREAM,
+                OrderContract.OrderEntry.COLUMN_HASTOPPING
+        };
+
+        return new CursorLoader(this, mCurrentCartUri,
+                projection,
+                null,
+                null,
+                null);
     }
 
     @Override
