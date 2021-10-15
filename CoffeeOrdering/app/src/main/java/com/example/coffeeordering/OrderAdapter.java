0f675f97759dbfaf1fb8a8561ba54a82d2c34d4d
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-15 11:44:35 +0530

    Update OrderAdapter.java

--- CoffeeOrdering/app/src/main/java/com/example/coffeeordering/OrderAdapter.java
+++ CoffeeOrdering/app/src/main/java/com/example/coffeeordering/OrderAdapter.java
@@ -1,4 +1,30 @@
 package com.example.coffeeordering;
 
-public class OrderAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> {
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
