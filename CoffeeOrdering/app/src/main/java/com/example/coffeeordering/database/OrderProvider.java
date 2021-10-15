cbc31a08e5b151926685325b6e2429093a241b81
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-15 17:24:55 +0530

    Update OrderProvider.java

--- CoffeeOrdering/app/src/main/java/com/example/coffeeordering/database/OrderProvider.java
+++ CoffeeOrdering/app/src/main/java/com/example/coffeeordering/database/OrderProvider.java
@@ -1,43 +1,130 @@
 package com.example.coffeeordering.database;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public class OrderProvider extends ContentProvider {
+
+    // this constant is needed in order to define the path of our modification in the table
+    public static final int ORDER = 100;
+
+    public static UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static {
+        sUriMatcher.addURI(OrderContract.CONTENT_AUTHORITY, OrderContract.PATH, ORDER);
+    }
+
+    public OrderHelper mHelper;
+
     @Override
     public boolean onCreate() {
-        return false;
+        mHelper = new OrderHelper(getContext());
+        return true;
     }
 
-    @Nullable
     @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+    public Cursor query(Uri uri,  String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
+        // since we are querying from the databse
+        SQLiteDatabase database = mHelper.getReadableDatabase();
+        Cursor cursor;
+        int match  = sUriMatcher.match(uri);
+        switch (match) {
+            case ORDER:
+                cursor = database.query(OrderContract.OrderEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+
+            default:
+                throw new IllegalArgumentException("CANT QUERY");
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Override
+    public String getType(Uri uri) {
         return null;
     }
 
-    @Nullable
+    // we will work on this method  because in this app we are just inserting the data not updating it for now
     @Override
-    public String getType(@NonNull Uri uri) {
-        return null;
+    public Uri insert(Uri uri,  ContentValues values) {
+
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ORDER:
+                return insertCart(uri, values);
+
+            default:
+                throw new IllegalArgumentException("Cant insert data");
+        }
+
+
     }
 
-    @Nullable
-    @Override
-    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        return null;
+    private Uri insertCart(Uri uri, ContentValues values) {
+
+        String name = values.getAsString(OrderContract.OrderEntry.COLUMN_NAME);
+        if(name == null) {
+            throw new IllegalArgumentException("Name is Required");
+        }
+
+        String quantity = values.getAsString(OrderContract.OrderEntry.COLUMN_QUANTITY);
+        if(quantity == null) {
+            throw new IllegalArgumentException("quantity is Required");
+        }
+
+        String price = values.getAsString(OrderContract.OrderEntry.COLUMN_PRICE);
+        if(price == null) {
+            throw new IllegalArgumentException("price is Required");
+        }
+
+        // SINCE WE ARE INSERTING DATA IN DATABASE SO NOW WE ARE WRITING ON DATABASE
+
+        SQLiteDatabase database = mHelper.getWritableDatabase();
+        long id = database.insert(OrderContract.OrderEntry.TABLE_NAME, null, values);
+
+        if (id == -1) {
+            return null;
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return ContentUris.withAppendedId(uri, id);
     }
 
     @Override
-    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
-        return 0;
+    public int delete( Uri uri,  String selection,  String[] selectionArgs) {
+
+        // we will use this to clear the data once order is made
+
+
+        int rowsDeleted;
+        SQLiteDatabase database = mHelper.getWritableDatabase();
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ORDER:
+                rowsDeleted = database.delete(OrderContract.OrderEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Cannot delete");
+        }
+
+        if (rowsDeleted!=0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsDeleted;
     }
 
     @Override
-    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
         return 0;
     }
 }
