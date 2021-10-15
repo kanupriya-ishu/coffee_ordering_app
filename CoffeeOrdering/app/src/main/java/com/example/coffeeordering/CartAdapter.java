136812954b08154c6b46e2f97689e0e311ca7a45
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-15 18:09:12 +0530

    Update CartAdapter.java

--- CoffeeOrdering/app/src/main/java/com/example/coffeeordering/CartAdapter.java
+++ CoffeeOrdering/app/src/main/java/com/example/coffeeordering/CartAdapter.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.coffeeordering.database.OrderContract;
 
 public class CartAdapter extends CursorAdapter {
 
@@ -20,6 +23,35 @@ public class CartAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        // getting theviews
+        TextView drinkName, yesCream, yesTopping, price, quantity;
+
+        drinkName = view.findViewById(R.id.drinkNameinOrderSummary);
+        price = view.findViewById(R.id.priceinOrderSummary);
+        yesCream = view.findViewById(R.id.hasCream);
+        yesTopping = view.findViewById(R.id.hasTopping);
+        quantity = view.findViewById(R.id.quantityinOrderSummary);
+
+        // getting the values by first getting the position of their columns
+
+        int name = cursor.getColumnIndex(OrderContract.OrderEntry.COLUMN_NAME);
+        int priceofdrink = cursor.getColumnIndex(OrderContract.OrderEntry.COLUMN_PRICE);
+        int quantityofdrink = cursor.getColumnIndex(OrderContract.OrderEntry.COLUMN_QUANTITY);
+        int hasCream = cursor.getColumnIndex(OrderContract.OrderEntry.COLUMN_CREAM);
+        int hasTopping = cursor.getColumnIndex(OrderContract.OrderEntry.COLUMN_HASTOPPING);
+
+
+        String nameofdrink = cursor.getString(name);
+        String pricesofdrink = cursor.getString(priceofdrink);
+        String quantitysofdrink = cursor.getString(quantityofdrink);
+        String yeshasCream = cursor.getString(hasCream);
+        String yeshastopping = cursor.getString(hasTopping);
+
+        drinkName.setText(nameofdrink);
+        price.setText(pricesofdrink);
+        yesCream.setText(yeshasCream);
+        yesTopping.setText(yeshastopping);
+        quantity.setText(quantitysofdrink);
 
     }
 }
