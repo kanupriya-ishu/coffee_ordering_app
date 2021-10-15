83da2e57788d6d8609e493fc6daa85da712b7272
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-15 17:35:14 +0530

    Added saveCart method

--- CoffeeOrdering/app/src/main/java/com/example/coffeeordering/InfoActivity.java
+++ CoffeeOrdering/app/src/main/java/com/example/coffeeordering/InfoActivity.java
@@ -1,7 +1,13 @@
 package com.example.coffeeordering;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,11 +18,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class InfoActivity extends AppCompatActivity {
+import com.example.coffeeordering.database.OrderContract;
+
+public class InfoActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     ImageView imageView;
     ImageButton plusquantity, minusquantity;
-    TextView quantitynumber, drinnkName, coffeePrice;
+    TextView quantitynumber, drinkName, coffeePrice;
     CheckBox addToppings, addExtraCream;
     Button addtoCart;
     int quantity;
@@ -32,20 +40,24 @@ public class InfoActivity extends AppCompatActivity {
         plusquantity = findViewById(R.id.addquantity);
         minusquantity  = findViewById(R.id.subquantity);
         quantitynumber = findViewById(R.id.quantity);
-        drinnkName = findViewById(R.id.drinkNameinInfo);
+        drinkName = findViewById(R.id.drinkNameinInfo);
         coffeePrice = findViewById(R.id.coffeePrice);
         addToppings = findViewById(R.id.addToppings);
         addtoCart = findViewById(R.id.addtocart);
         addExtraCream = findViewById(R.id.addCream);
 
         // setting the name of drink
-        drinnkName.setText("Green Tea");
+        drinkName.setText("Green Tea");
 
         addtoCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(InfoActivity.this, SummaryActivity.class);
                 startActivity(intent);
+
+                // once this button is clicked we want to save our values in the database and send those values
+                // right away to summary activity where we display the order info
+                saveCart();
             }
         });
 
@@ -85,6 +97,45 @@ public class InfoActivity extends AppCompatActivity {
         });
     }
 
+    private boolean saveCart() {
+        // getting the values from our views
+        String name = drinkName.getText().toString();
+        String price = coffeePrice.getText().toString();
+        String quantity = quantitynumber.getText().toString();
+
+        ContentValues values = new ContentValues();
+        values.put(OrderContract.OrderEntry.COLUMN_NAME, name);
+        values.put(OrderContract.OrderEntry.COLUMN_PRICE, price);
+        values.put(OrderContract.OrderEntry.COLUMN_QUANTITY, quantity);
+
+        if (addExtraCream.isChecked()) {
+            values.put(OrderContract.OrderEntry.COLUMN_CREAM, "Has Cream: YES");
+        } else {
+            values.put(OrderContract.OrderEntry.COLUMN_CREAM, "Has Cream: NO");
+
+        }
+
+        if (addToppings.isChecked()) {
+            values.put(OrderContract.OrderEntry.COLUMN_HASTOPPING, "Has Toppings: YES");
+        } else {
+            values.put(OrderContract.OrderEntry.COLUMN_HASTOPPING, "Has Toppings: NO");
+
+        }
+
+        if (mCurrentCartUri == null) {
+            Uri newUri = getContentResolver().insert(OrderContract.OrderEntry.CONTENT_URI, values);
+            if (newUri==null) {
+                Toast.makeText(this, "Failed to add to Cart", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Success  adding to Cart", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+        hasAllRequiredValues = true;
+        return hasAllRequiredValues;
+    }
+
     private int calculatePrice(CheckBox addExtraCream, CheckBox addToppings) {
         int basePrice = 5;
 
@@ -103,5 +154,21 @@ public class InfoActivity extends AppCompatActivity {
 
     private void displayQuantity() {
         quantitynumber.setText(String.valueOf(quantity));
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 }
