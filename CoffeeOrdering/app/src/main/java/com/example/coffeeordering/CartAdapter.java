dfc742a96b2d684810834d46dfe03581ab0e937c
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-15 18:02:04 +0530

    Updated newView method

--- CoffeeOrdering/app/src/main/java/com/example/coffeeordering/CartAdapter.java
+++ CoffeeOrdering/app/src/main/java/com/example/coffeeordering/CartAdapter.java
@@ -2,19 +2,20 @@ package com.example.coffeeordering;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 
 public class CartAdapter extends CursorAdapter {
-    
+
     public CartAdapter(Context context, Cursor cursor) {
         super(context, cursor, 0);
     }
 
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-        return null;
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.cartlist, parent, false);
     }
 
     @Override
